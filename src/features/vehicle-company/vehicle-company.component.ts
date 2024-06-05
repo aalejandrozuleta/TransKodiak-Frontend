4f4f8c3868d53ed871ace17213b532c2c99fa398
Alejo Zuleta <jhoitan266@gmail.com>
2024-06-05 02:58:42 -0500

    merge default

--- src/features/vehicle-company/vehicle-company.component.ts
+++ src/features/vehicle-company/vehicle-company.component.ts
@@ -1,32 +1,26 @@
-<<<<<<< HEAD
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { CompleteSideBarComponent } from '@organisms/complete-side-bar/complete-side-bar.component';
 import { NavBarVehicleCompanyComponent } from '@templates/nav-bar-vehicle-company/nav-bar-vehicle-company.component';
-=======
-import { userTableVehicle } from './../../core/models/user-table-vehicle-company';
-import { GroupTableUserVehicleCompanyComponent } from 'src/shared/components/organisms/group-table-user-vehicle-company/group-table-user-vehicle-company.component';
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {GroupWelcomeVehicleCompanyComponent} from 'src/shared/components/organisms/group-welcome-vehicle-company/group-welcome-vehicle-company.component';
->>>>>>> origin/feature/WelcomeBlockVehicleCompany
+import { GroupWelcomeVehicleCompanyComponent } from 'src/shared/components/organisms/group-welcome-vehicle-company/group-welcome-vehicle-company.component';
+import { GroupTableUserVehicleCompanyComponent } from '@organisms/group-table-user-vehicle-company/group-table-user-vehicle-company.component';
+
+
 
 @Component({
   selector: 'app-vehicle-company',
   standalone: true,
-<<<<<<< HEAD
-  imports: [CommonModule,NavBarVehicleCompanyComponent,CompleteSideBarComponent],
-=======
-  imports: [CommonModule, 
+  imports: [
+    CommonModule,
+    NavBarVehicleCompanyComponent,
+    CompleteSideBarComponent,
     GroupWelcomeVehicleCompanyComponent,
-    GroupTableUserVehicleCompanyComponent
-    ],
->>>>>>> origin/feature/WelcomeBlockVehicleCompany
+    GroupTableUserVehicleCompanyComponent,
+  ],
+
   templateUrl: './vehicle-company.component.html',
-  styleUrl: './vehicle-company.component.scss'
+  styleUrl: './vehicle-company.component.scss',
 })
 export class VehicleCompanyComponent {
   // @Input() users!: User[];
 }
-
-
