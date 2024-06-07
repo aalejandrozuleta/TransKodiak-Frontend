d08afd859d36c0578b158cce78bdf9f8cedef9ae
Sofiaaa20 <126484803+Sofiaaa20@users.noreply.github.com>
2024-06-07 11:52:36 -0500

    Creation of the delete service

--- src/core/services/administration-user-vehicle-company.service.ts
+++ src/core/services/administration-user-vehicle-company.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { UserVehicleCompanyService } from './user-vehicle-company.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdministrationUserVehicleCompanyService {
 
-  constructor() { }
+  constructor(private userVehicleCompanyService: UserVehicleCompanyService) { }
+
+  deleteUser(userId: number) {
+    const users = this.userVehicleCompanyService.getUsers();
+    const updatedUsers = users.filter(user => user.id !== userId);
+    this.userVehicleCompanyService.setUsers(updatedUsers);
+  }
 }
