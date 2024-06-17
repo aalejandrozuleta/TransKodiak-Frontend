3d0d1e89216dd3132e3f07d6aead95242b8a50f6
aalejandrozuleta <jhoitan266@gmail.com>
2024-06-17 16:48:26 -0500

    feature transporter register

---
+++ src/core/models/userTransporter.ts
@@ -0,0 +1,8 @@
+export interface userTransporterCreate {
+  // avatar:string;
+  name: string;
+  email:string;
+  phone:string;
+  license:string;
+  status?:string
+}
--- src/core/services/administration-user-vehicle-company.service.ts
+++ src/core/services/administration-user-vehicle-company.service.ts
@@ -1,17 +1,22 @@
+import { userTransporterCreate } from './../models/userTransporter';
 import { Injectable } from '@angular/core';
 import { UserVehicleCompanyService } from './user-vehicle-company.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdministrationUserVehicleCompanyService {
-
-  constructor(private userVehicleCompanyService: UserVehicleCompanyService) { }
+  constructor(private userVehicleCompanyService: UserVehicleCompanyService) {}
 
   deleteUser(userId: number) {
     this.userVehicleCompanyService.deleteUser(userId).subscribe(() => {
       console.log('Usuario eliminado');
     });
   }
-  
+
+  registerTransporter(user: userTransporterCreate) {
+    this.userVehicleCompanyService.createUserTransporter(user).subscribe(() => {
+      console.log('Usuario registrado');
+    });
+  }
 }
