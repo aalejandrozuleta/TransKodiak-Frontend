8645118786baef2f2afd566ffa74cb9f12bad670
Sofiaaa20 <126484803+Sofiaaa20@users.noreply.github.com>
2024-07-07 17:29:21 -0500

    Create Form Register Company Vehicular

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '@feature/home/home.component';
+import { RegisterFormCompanyComponent } from '@feature/register-form-company/register-form-company.component';
 import { VehicleCompanyComponent } from '@feature/vehicle-company/vehicle-company.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'vehicleCompany', component: VehicleCompanyComponent },
+  { path: 'registerCompany', component: RegisterFormCompanyComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
--- src/shared/components/organisms/group-form-register-company/group-form-register-company.component.ts
+++ src/shared/components/organisms/group-form-register-company/group-form-register-company.component.ts
@@ -4,12 +4,20 @@ import { ImgFormRegisterCompanyComponent } from '@atoms/img-form-register-compan
 import { ButtonsFormRegisterCompanyComponent } from '@molecules/buttons-form-register-company/buttons-form-register-company.component';
 import { InputsFormRegisterCompanyComponent } from '@molecules/inputs-form-register-company/inputs-form-register-company.component';
 import { userRegister } from '@models/userRegister';
+import { TittleFormRegisterCompanyComponent } from '@atoms/tittle-form-register-company/tittle-form-register-company.component';
+import { TextFormRegisterCompanyComponent } from '@atoms/text-form-register-company/text-form-register-company.component';
 
 
 @Component({
   selector: 'app-group-form-register-company',
   standalone: true,
-  imports: [CommonModule, ImgFormRegisterCompanyComponent,ButtonsFormRegisterCompanyComponent,InputsFormRegisterCompanyComponent],
+  imports: [CommonModule, 
+    ImgFormRegisterCompanyComponent,
+    ButtonsFormRegisterCompanyComponent,
+    InputsFormRegisterCompanyComponent,
+    TittleFormRegisterCompanyComponent,
+    TextFormRegisterCompanyComponent
+  ],
   templateUrl: './group-form-register-company.component.html',
   styleUrl: './group-form-register-company.component.scss'
 })
