a3772aa9fcb421c12b1169cf420f3d287452543e
Sofiaaa20 <126484803+Sofiaaa20@users.noreply.github.com>
2024-07-11 08:08:37 -0500

    Correction Type Input

--- src/shared/components/organisms/group-form-register-company/group-form-register-company.component.ts
+++ src/shared/components/organisms/group-form-register-company/group-form-register-company.component.ts
@@ -63,7 +63,7 @@ export class GroupFormRegisterCompanyComponent {
     {
       label: 'contraseña',
       id: 'password',
-      type: 'text',
+      type: 'password',
       placeholder: 'Contraseña',
       fild: 'password'
     },
